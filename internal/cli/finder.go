b6257daa8b05e90948eff6ab4da1aec5167527b0
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-08-28 12:08:12 -0500

    Create the flags and methods of the find command, and inject the student service into NewFider.
    issue #4

--- internal/cli/finder.go
+++ internal/cli/finder.go
@@ -3,24 +3,42 @@ package cli
 import (
 	"context"
 	"flag"
-	"fmt"
+
+	"github.com/erik-sostenes/gofige/internal/model"
+	"github.com/erik-sostenes/gofige/internal/services"
 )
 
 type (
 	// Finder contains methods to search data in a collection
 	Finder interface {
-		// Find find all data
-		Find(context.Context)
+		// Find find all data and returns a .csv file
+		Find(context.Context) (model.Students, error)
+		// FindByFlags find any collection that matches the flags
+		// returns a .csv file
+		FindByFlags(context.Context, model.Student) (model.Students, error)
 	}
 	finder struct {
-		fs *flag.FlagSet
+		fs             *flag.FlagSet
+		studentService services.StudentService
+		path           string
+		all            bool
+		studentFlags   model.Student
 	}
 )
 
-func NewFinder() *finder {
+func NewFinder(studentService services.StudentService) *finder {
 	f := &finder{
-		fs: flag.NewFlagSet("find", flag.ContinueOnError),
+		fs:             flag.NewFlagSet("find", flag.ContinueOnError),
+		studentService: studentService,
 	}
+	f.fs.BoolVar(&f.all, "all", false, "find all the documents of the collection")
+
+	f.fs.StringVar(&f.path, "path", "./", "addres when you want to save the .csv file with the data.")
+	f.fs.StringVar(&f.studentFlags.Tuition, "tuition", "nil", "find the collection that matches with tuition")
+	f.fs.StringVar(&f.studentFlags.Name, "name", "nil", "find the collection that matches with name")
+	f.fs.StringVar(&f.studentFlags.Carrer, "carreer", "nil", "find the collection that matches with carreer")
+	f.fs.StringVar(&f.studentFlags.Grade, "grade", "nil", "find the collection that matches with grade")
+	f.fs.StringVar(&f.studentFlags.Group, "group", "nil", "find the collection that matches with group")
 	return f
 }
 
@@ -32,7 +50,19 @@ func (f *finder) Parse(args []string) error {
 	return f.fs.Parse(args)
 }
 
-func (f *finder) Run() error {
-	fmt.Println("Finder")
-	return nil
+func (f *finder) Run() (err error) {
+	if f.all {
+		_, err = f.Find(context.TODO())
+		return
+	}
+	_, err = f.FindByFlags(context.TODO(), f.studentFlags)
+	return
+}
+
+func (f *finder) Find(ctx context.Context) (model.Students, error) {
+	return f.studentService.Find(ctx)
+}
+
+func (f *finder) FindByFlags(ctx context.Context, studentFlags model.Student) (model.Students, error) {
+	return f.studentService.FindByFlags(ctx, studentFlags)
 }
