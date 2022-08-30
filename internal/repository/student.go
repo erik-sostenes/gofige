fe8880fde26466394b639e6dadaba4787db0af99
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-08-30 01:04:17 -0500

    Creates the functionality to find a collection by arguments.
    Note: Implement unit test.
    
    issue #5

--- internal/repository/student.go
+++ internal/repository/student.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/erik-sostenes/gofige/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,6 +13,10 @@ type (
 	StudentStorer interface {
 		// Insert method that inserts one document with collections to mongodb
 		Insert(context.Context, model.Students) error
+		// Find method that find one document with collections to mongodb
+		Find(context.Context, map[string]string) (model.Students, error)
+		// Delete
+		Delete(context.Context, map[string]string) error
 	}
 	// studentStorer studentStorer will connect and work with the mongo client
 	studentStorer struct {
@@ -37,6 +42,37 @@ func (s *studentStorer) Insert(ctx context.Context, students model.Students) (er
 	return
 }
 
+// Find gets a document depending on the filter conditions
+// the filter will be through the flags
+func (s *studentStorer) Find(ctx context.Context, flags map[string]string) (students model.Students, err error) {
+	cur, err := s.db.Find(ctx, flags)
+	if err != nil {
+		return
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var student model.Student
+		if err = cur.Decode(&student); err != nil {
+			return
+		}
+		students = append(students, student)
+	}
+	if err = cur.Err(); err != nil {
+		return
+	}
+	if len(students) == 0 {
+		err = mongo.ErrNoDocuments
+		return
+	}
+	return
+}
+
+func (s *studentStorer) Delete(ctx context.Context, _ map[string]string) (err error) {
+	_, err = s.db.DeleteMany(ctx, bson.M{})
+	return
+}
+
 // _ "implement" constraint for *MockStudentStorer
 var _ StudentStorer = (*MockStudentStorer)(nil)
 
@@ -52,7 +88,12 @@ func (m *MockStudentStorer) Insert(ctx context.Context, students model.Students)
 	return m.Error
 }
 
-// Find search a model.Students by flags
+// Find search a model.Students by arguments
 func (m *MockStudentStorer) Find(context.Context, map[string]string) (model.Students, error) {
 	return m.MockStudents, m.Error
+}
+
+// Delete delete a model.Students by arguments
+func (m *MockStudentStorer) Delete(context.Context, map[string]string) (err error) {
+	return
 }
--- internal/repository/student_test.go
+++ internal/repository/student_test.go
@@ -2,10 +2,15 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/erik-sostenes/gofige/internal/model"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type arguments map[string]string
 
 func TestStudentStorer_Insert(t *testing.T) {
 	var (
@@ -31,26 +36,92 @@ func TestStudentStorer_Insert(t *testing.T) {
 
 	tsc := map[string]struct {
 		studentStorer StudentStorer
-		students      model.Students
+		mockStudents  model.Students
 		expectedError error
 	}{
-		"Given a successful insertion in mongodb, the data is stored in the collection students_one": {
-			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students_one")),
-			students:      mockStudents,
+		"Given a successful insertion in mongodb, the data is stored in the collection students": {
+			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students")),
+			mockStudents:  mockStudents,
 			expectedError: nil,
 		},
 		"Given a successful insertion in mongodb, the data is stored in the collection students_two": {
 			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students_two")),
-			students:      mockStudents,
+			mockStudents:  mockStudents,
 			expectedError: nil,
 		},
 	}
 
 	for name, ts := range tsc {
 		t.Run(name, func(t *testing.T) {
-			err := ts.studentStorer.Insert(context.TODO(), ts.students)
-			if err != nil {
+			err := ts.studentStorer.Insert(context.TODO(), ts.mockStudents)
+			if err != ts.expectedError {
 				t.Errorf("expected error %s, got error %s", ts.expectedError, err)
+			}
+		})
+	}
+}
+
+func TestStudentStorer_Find(t *testing.T) {
+	var (
+		mockStudentOne = model.Student{
+			Tuition: "X189HR",
+			Name:    "Erik Sostenes",
+			Grade:   "5",
+			Group:   "A",
+			Carrer:  "Ingenieria",
+		}
+		mockStudentTwo = model.Student{
+			Tuition: "EVS765",
+			Name:    "Erik Simon",
+			Grade:   "7",
+			Group:   "B",
+			Carrer:  "Sistemas",
+		}
+	)
+
+	tsc := map[string]struct {
+		studentStorer   StudentStorer
+		expetedStudents model.Students
+		arguments       map[string]string
+		expectedError   error
+	}{
+		"Given a successful search, it returns a slice of students": {
+			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students")),
+			expetedStudents: model.Students{
+				mockStudentOne,
+				mockStudentTwo,
+			},
+		},
+		"Given a successful search with arguments, it returns a slice of students": {
+			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students")),
+			expetedStudents: model.Students{
+				mockStudentOne,
+			},
+			arguments: arguments{"tuition": mockStudentOne.Tuition},
+		},
+		"Given a search that the collection does not contains data, returns a mongo.ErrNoDocuments error": {
+			studentStorer: NewStudentStorer(NewMDB(Config).Collection("students")),
+			expectedError: *&mongo.ErrNoDocuments,
+		},
+	}
+
+	for name, ts := range tsc {
+		ts := ts
+		t.Run(name, func(t *testing.T) {
+			_ = ts.studentStorer.Insert(context.TODO(), ts.expetedStudents)
+
+			t.Cleanup(func() {
+				_ = ts.studentStorer.Delete(context.TODO(), ts.arguments)
+			})
+
+			mock, err := ts.studentStorer.Find(context.TODO(), ts.arguments)
+			if !errors.Is(err, ts.expectedError) {
+				t.Errorf("expected error %s, got error %s", ts.expectedError, err)
+				t.SkipNow()
+			}
+
+			if !reflect.DeepEqual(mock, ts.expetedStudents) {
+				t.Errorf("expeted %v,\n got %v", ts.expetedStudents, mock)
 			}
 		})
 	}
